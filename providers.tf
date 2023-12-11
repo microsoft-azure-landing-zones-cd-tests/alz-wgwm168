5c0748481a9a7c4eda4aa2b4b196eb50cc01f3fd
jared.holgate@microsoft.com <jared.holgate@microsoft.com>
2023-12-11 18:38:44 +0000

    Add providers.tf [skip ci]

---
+++ providers.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.61.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azurerm" {
+  alias           = "management"
+  subscription_id = var.subscription_id_management
+  features {}
+}
+
+provider "azurerm" {
+  alias           = "connectivity"
+  subscription_id = var.subscription_id_connectivity
+  features {}
+}
+
+provider "azurerm" {
+  alias           = "identity"
+  subscription_id = var.subscription_id_identity
+  features {}
+}
